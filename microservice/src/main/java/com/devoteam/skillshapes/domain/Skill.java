dfa584076edd78a315f6a290a619c990e84595ae
DM05744 <david.minkovski@devoteam.com>
2021-03-15 15:27:10 +0100

    Indexed Skill

--- microservice/src/main/java/com/devoteam/skillshapes/domain/Skill.java
+++ microservice/src/main/java/com/devoteam/skillshapes/domain/Skill.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.hibernate.search.engine.backend.types.Sortable;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.KeywordField;
 
 import javax.persistence.*;
@@ -21,6 +22,7 @@ import java.io.Serializable;
 @Table(name = "skill")
 @Cacheable
 @RegisterForReflection
+@Indexed
 public class Skill extends PanacheEntityBase implements Serializable {
 
     private static final long serialVersionUID = 1L;
