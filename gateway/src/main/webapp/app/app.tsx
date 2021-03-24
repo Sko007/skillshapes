9ce4dda26dc89d56579d4f350b9d9a55c71c6dd0
DM05744 <david.minkovski@devoteam.com>
2021-03-24 16:30:22 +0100

    Dashboard for Skillshapes

--- gateway/src/main/webapp/app/app.tsx
+++ gateway/src/main/webapp/app/app.tsx
@@ -1,7 +1,7 @@
 import 'react-toastify/dist/ReactToastify.css';
 import './app.scss';
 
-import React, { useEffect } from 'react';
+import React, { createContext, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Card } from 'reactstrap';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -22,6 +22,8 @@ import AppRoutes from 'app/routes';
 const baseHref = document.querySelector('base').getAttribute('href').replace(/\/$/, '');
 
 export interface IAppProps extends StateProps, DispatchProps {}
+
+export const AdminContext = React.createContext(false);
 
 export const App = (props: IAppProps) => {
   useEffect(() => {
@@ -46,12 +48,14 @@ export const App = (props: IAppProps) => {
           />
         </ErrorBoundary>
         <div className="container-fluid view-container" id="app-view-container">
-          { /* <Card className="jh-card"> */ }
-            <ErrorBoundary>
+          {/* <Card className="jh-card"> */}
+          <ErrorBoundary>
+            <AdminContext.Provider value={props.isAdmin}>
               <AppRoutes />
-            </ErrorBoundary>
-          { /* </Card> */ }
-         { /* <Footer /> */ }
+            </AdminContext.Provider>
+          </ErrorBoundary>
+          {/* </Card> */}
+          {/* <Footer /> */}
         </div>
       </div>
     </Router>
