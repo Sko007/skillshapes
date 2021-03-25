203326262c922990bd46a21d01d1ee160e46e5ed
DM05744 <david.minkovski@devoteam.com>
2021-03-25 17:44:55 +0100

    Health Endpoint Eureka Microservice

--- microservice/src/main/java/com/devoteam/skillshapes/web/rest/HealthResource.java
+++ microservice/src/main/java/com/devoteam/skillshapes/web/rest/HealthResource.java
@@ -5,12 +5,35 @@ import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.Liveness;
 import org.eclipse.microprofile.health.Readiness;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Map;
+
 @Liveness
+@Path("/info")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class HealthResource implements HealthCheck {
 
     @Override
     public HealthCheckResponse call() {
         return HealthCheckResponse.up("I'm alive");
+    }
+
+    @GET
+    @Path("/health")
+    public Response health() {
+        return Response.ok(Map.of("STATUS", "UP")).build();
+    }
+
+    @GET
+    @Path("/status")
+    public Response status() {
+        return Response.ok(Map.of()).build();
     }
 }
 
