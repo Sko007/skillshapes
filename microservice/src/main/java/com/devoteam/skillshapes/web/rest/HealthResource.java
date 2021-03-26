1ef1e7c8a29fe47329bd0d6713013efe1208adaf
DM05744 <david.minkovski@devoteam.com>
2021-03-26 11:06:06 +0100

    Added Health Check Logger and Endpoint

--- microservice/src/main/java/com/devoteam/skillshapes/web/rest/HealthResource.java
+++ microservice/src/main/java/com/devoteam/skillshapes/web/rest/HealthResource.java
@@ -1,17 +1,28 @@
 package com.devoteam.skillshapes.web.rest;
 
+import io.quarkus.eureka.config.DefaultInstanceInfoContext;
+import io.quarkus.eureka.config.EurekaRuntimeConfiguration;
+import io.quarkus.eureka.config.InstanceInfoContext;
+import io.quarkus.eureka.exception.HealthCheckException;
+import io.quarkus.runtime.annotations.ConfigItem;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.Liveness;
 import org.eclipse.microprofile.health.Readiness;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import javax.ws.rs.client.Client;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Map;
+import io.quarkus.eureka.client.Status;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static javax.ws.rs.core.Response.Status.Family.CLIENT_ERROR;
+import static javax.ws.rs.core.Response.Status.Family.SUCCESSFUL;
 
 @Liveness
 @Path("/info")
@@ -35,5 +46,56 @@ public class HealthResource implements HealthCheck {
     public Response status() {
         return Response.ok(Map.of()).build();
     }
+
+    private final Logger log = LoggerFactory.getLogger(SkillShapeResource.class);
+
+    @GET
+    @Path("/check")
+    public Response healthCheck(@QueryParam("url") String healthCheckUrl) {
+        Client client = ResteasyClientBuilder.newClient();
+        try {
+            log.info("Health Check for: {}",healthCheckUrl);
+            Response response = client.target(healthCheckUrl)
+                .request(MediaType.APPLICATION_JSON_TYPE)
+                .get();
+            if(response != null){
+                log.info("Response: {}",response.getHeaders().toString());
+                log.info("Response Status: {}",response.getStatusInfo());
+            }
+            if (response.getStatusInfo().getFamily().equals(CLIENT_ERROR)) {
+                throw new HealthCheckException(
+                    "Instance can't reach own application health check. Ensure this has been implemented"
+                );
+            }
+
+            Status status = getStatusFromResponse(response);
+
+            return Response.ok(Map.of("STATUS",status.toString())).build();
+
+        } catch (ProcessingException ex) {
+            throw new HealthCheckException("Health check not reachable: "+healthCheckUrl, ex);
+        } finally {
+            client.close();
+        }
+    }
+
+    private Status getStatusFromResponse(final Response response) {
+
+        if (!response.getStatusInfo().getFamily().equals(SUCCESSFUL)) {
+            return Status.DOWN;
+        }
+
+        final Map<String, String> body = response.readEntity(Map.class);
+        log.info("Health Check GetStatusFromResponse: {}", body);
+        log.info("Health Check GetStatusFromResponse Keyset: {}", body.keySet());
+        return body.entrySet()
+            .stream()
+            .filter(e -> e.getKey().equalsIgnoreCase("status"))
+            .map(Map.Entry::getValue)
+            .map(String::toUpperCase)
+            .map(Status::valueOf)
+            .findFirst().orElse(Status.UNKNOWN);
+    }
+
 }
 
